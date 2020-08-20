fe1e0a6b9ebe89477bd741b3fbd8fdc8e88b649f
dfj <dfj@qq.com>
2020-08-20 18:02:59 +0800

    创建mvc

--- src/main/java/com/dfj/dao/UserDao.java
+++ src/main/java/com/dfj/dao/UserDao.java
@@ -9,7 +9,4 @@ public interface UserDao {
 
     @Select("select * from cn_user")
     List<User> findAll();
-
-    @Select("select * from cn_user where cn_user_name = #{cn_user_name}")
-    User findByName(String cn_user_name);
 }
